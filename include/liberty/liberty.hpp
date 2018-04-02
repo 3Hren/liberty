89ec8ad4a61bb47d849da0d43c35c7cb07b00516
karitra <karev.alexandre@gmail.com>
2018-04-02 13:36:52 +0300

    refact(variant #2): ensure `call once` semantics for callback

--- include/liberty/liberty.hpp
+++ include/liberty/liberty.hpp
@@ -33,13 +33,10 @@ public:
     template<typename F>
     auto set_complete_callback(F fn) -> void {
         std::unique_ptr<F> holder(new F(std::move(fn)));
-
-        const auto deleter = +[] (void* data) { delete static_cast<F*>(data); };
-        const auto completion = +[] (const liberty_http_error* error, const liberty_http_response* response, void* data) {
-            (*static_cast<F*>(data))(error, response);
-        };
-
-        liberty_http_request_complete_callback(request, completion, holder.release(), deleter);
+        liberty_http_request_complete_callback(request, [](const liberty_http_error* error, const liberty_http_response* response, void* data) {
+            std::unique_ptr<F> holder(static_cast<F*>(data));
+            (*holder)(error, response);
+        }, holder.release());
     }
 
     auto into_inner() && -> liberty_http_request* {
